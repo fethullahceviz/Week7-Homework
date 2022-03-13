9734fa6d0dccbc77df7c6da5d5cc4514536a06ab
Fethullah Ceviz <96957283+fethullahceviz@users.noreply.github.com>
2022-03-13 20:27:47 +0100

    Update w7_hw3.py

--- fceviz_hw_7/w7_hw3.py
+++ fceviz_hw_7/w7_hw3.py
@@ -1,12 +1,12 @@
-"""Write a program that list according to email addresses without email domains in a text.
+"""
+Write a program that list according to email addresses without email domains in a text.
 Example:
 Input:
-The advencements in biomarine studies franky@google.com with the 
-investments necessary and Davos sinatra123@yahoo.com. 
-Then New Yorker article on wind farms...
+The advencements in biomarine studies franky@google.com with the investments necessary and Davos sinatra123@yahoo.com.Then New Yorker article on wind farms...
 Output :
 franky
-sinatra123"""
+sinatra123
+"""
 import re
 for match in re.finditer(r"\b\w+@",input("Enter a text :")):
     print(match.group().split("@")[0])
